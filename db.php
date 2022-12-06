7492c51e74cd5d72944aba0f939406e228878e8b
Mazulj11 <116559174+Mazulj11@users.noreply.github.com>
2022-12-06 16:20:07 +0100

    Add files via upload

---
+++ db.php
@@ -0,0 +1,27 @@
+<?php
+ini_set("display_startup_errors", 1);
+ini_set("display_errors", 1);
+error_reporting(-1);
+
+$db_host="localhost";
+$db_user = "root";
+$db_password = "";
+$db_name = "projekt";
+
+$mysqli = new mysqli($db_host,$db_user,$db_password,$db_name);
+
+if($mysqli->connect_errno) {
+	echo "Failed to connect to MySQL: " .$mysqli -> connect_error;
+	exit();
+} else {
+	echo "Uspjesno ste spojeni na bazu!";
+	$sql = "SELECT * FROM users";
+	
+	$result = $mysqli->query($sql);
+	
+	if ($result) {
+		$row = $result -> fetch_row();
+		echo json_encode($row);
+  }
+}
+?>
